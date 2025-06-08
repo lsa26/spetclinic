bbdf2f8e6712a48b794937d2a3268d98019f664a
lsa26 <151360195+lsa26@users.noreply.github.com>
2025-06-09 01:30:00 +0200

    Update feature.java

--- src/main/java/com/example/feature.java
+++ src/main/java/com/example/feature.java
@@ -1,4 +1,14 @@
 package com.example;
-public class feature {
-    // ... rest of the code ... 
-}
+
+public class Feature {
+
+    public static void main(String[] args) {
+        System.out.println("This will compile!");
+
+        int result = addNumbers(5, 10);
+        System.out.println("Result: " + result);
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+
